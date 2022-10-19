a280c4e82242dbbeb74362b06320ce41e482c058
RicardoGomes212 <116182791+RicardoGomes212@users.noreply.github.com>
2022-10-19 22:38:46 +0100

    change-password
    Foi adicionada uma feature para poder ser feita a mudança de password localizada na dropdownlist do layout.

--- twitter-exercicio/routes/web.php
+++ twitter-exercicio/routes/web.php
@@ -19,4 +19,11 @@ Route::get('/', function () {
 
 Auth::routes();
 
+//default home route
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+//route to change password
+Route::get('/change-password', [App\Http\Controllers\HomeController::class, 'changePassword'])->name('change-password');
+
+//update password
+Route::post('/change-password', [App\Http\Controllers\HomeController::class, 'updatePassword'])->name('update-password');
