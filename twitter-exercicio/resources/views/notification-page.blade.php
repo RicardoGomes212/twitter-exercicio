6a32034c8c91c51268515370124ed13ab3516d23
RicardoGomes212 <116182791+RicardoGomes212@users.noreply.github.com>
2022-10-20 22:16:17 +0100

    Update notification-page.blade.php

--- twitter-exercicio/resources/views/notification-page.blade.php
+++ twitter-exercicio/resources/views/notification-page.blade.php
@@ -5,7 +5,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">{{ __('Notificações') }}</div>
+                    <div class="card-header">{{ __('Notifications') }}</div>
                     
                     <ul class="list-group">
                         <li class="list-group-item disabled">Cras justo odio</li>
